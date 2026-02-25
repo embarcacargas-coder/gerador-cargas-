da9fe105e76c9a75ad504d05639133ada11de1ea
embarcacargas-coder <embarcacargas@gmail.com>
2026-02-24 21:14:20 -0300

    Create html-matrix.js

---
+++ template/html-matrix.js
@@ -0,0 +1,15 @@
+const templateHTML = `
+<div class="carga">
+  <h2>{{NOME}}</h2>
+  <p><strong>Origem:</strong> {{ORIGEM}}</p>
+  <p><strong>Destino:</strong> {{DESTINO}}</p>
+  <p><strong>Peso:</strong> {{PESO}}</p>
+  <p><strong>Valor do Frete:</strong> {{VALOR}}</p>
+  <p><strong>Veículo:</strong> {{VEICULO}}</p>
+
+  <hr>
+
+  <p><strong>Agenciador:</strong> {{AGENCIADOR}}</p>
+  <p><strong>Contato:</strong> {{TELEFONE}}</p>
+</div>
+`;
